99e30f361d85335c3f5affd91b5e9adf7cb7ff8f
Thang Huynh-Quoc <thanghq@droup.co>
2020-09-07 15:20:24 +0700

    refactor: delete unused code (quotings and solar panels)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthencationModule } from 'src/authentication/authentication.module';
 import { RoleModule } from 'src/roles/role.module';
-import { SolarPanelModule } from 'src/solar-panels/solar-panel.module';
 import { UserModule } from 'src/users/user.module';
-import { QuotingModule } from '../quotings/quoting.module';
 import { SystemDesignModule } from '../system-designs/system-design.module';
 import { ExternalServiceModule } from './../external-services/external-service.module';
 import { ProductModule } from './../products/product.module';
@@ -15,11 +13,9 @@ import { ProductModule } from './../products/product.module';
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRoot(process.env.MONGO_URL),
-    SolarPanelModule,
     UserModule,
     RoleModule,
     AuthencationModule,
-    QuotingModule,
     SystemDesignModule,
     ProductModule,
     ExternalServiceModule,
--- src/quotings/constants.ts
+++
@@ -1,4 +0,0 @@
-export enum ORIENTATION {
-  LANDSCAPE = 'Landscape',
-  PORTRAIT = 'Portrait',
-}
