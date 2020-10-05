87f7c04e6b5105a4683086a9514fd214a8b51741
Thang Huynh-Quoc <quocthang.it247@gmail.com>
2020-10-05 16:43:16 +0700

    fix: expose get list api for funding sources

--- src/funding-sources/funding-source.service.ts
+++ src/funding-sources/funding-source.service.ts
@@ -1,11 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { OperationResult, Pagination } from 'src/app/common';
 import { FundingSource, FUNDING_SOURCE } from './funding-source.schema';
+import { FundingSourceDto } from './res/funding-source.dto';
 
 @Injectable()
 export class FundingSourceService {
   constructor(@InjectModel(FUNDING_SOURCE) private fundingSource: Model<FundingSource>) {}
+
+  async getList(limit: number, skip: number): Promise<OperationResult<Pagination<FundingSourceDto>>> {
+    const [fundingSources, total] = await Promise.all([
+      this.fundingSource.find().limit(limit).skip(skip),
+      this.fundingSource.countDocuments(),
+    ]);
+
+    return OperationResult.ok({
+      data: fundingSources.map(fundingSource => new FundingSourceDto(fundingSource)),
+      total,
+    });
+  }
 
   // ->>>>>>>>> INTERNAL <<<<<<<<<<-
 
