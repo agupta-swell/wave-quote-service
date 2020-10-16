565d760380364b8f780a89e45377add186fa1ac0
Thang Huynh-Quoc <quocthang.it247@gmail.com>
2020-10-16 15:05:17 +0700

    fix: get latest quote when previous step has changed and don't save into DB

--- src/quotes/quote.service.ts
+++ src/quotes/quote.service.ts
@@ -1,6 +1,6 @@
 import { forwardRef, Inject, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { groupBy, pickBy, sumBy } from 'lodash';
+import { groupBy, pick, pickBy, sumBy } from 'lodash';
 import { Model } from 'mongoose';
 import { FundingSourceService } from 'src/funding-sources/funding-source.service';
 import { UtilityProgramService } from 'src/utility-programs/utility-program.service';
@@ -12,6 +12,7 @@ import { SystemDesignService } from './../system-designs/system-design.service';
 import { FINANCE_PRODUCT_TYPE } from './constants';
 import { Quote, QUOTE, QuoteModel } from './quote.schema';
 import { CalculateQuoteDetailDto, CreateQuoteDto, UpdateQuoteDto } from './req';
+import { LoanProductAttributesDto } from './req/sub-dto';
 import { QuoteDto } from './res/quote.dto';
 import { CalculationService } from './sub-services/calculation.service';
 
@@ -108,6 +109,12 @@ export class QuoteService {
       utilityProgram: {
         utilityProgramId: utilityProgram?.id,
         utilityProgramName: utilityProgram?.name,
+        rebateAmount: utilityProgram?.rebate_amount,
+        utility_program_data_snapshot: {
+          id: utilityProgram?.id,
+          name: utilityProgram?.name,
+        },
+        utility_program_data_snapshot_date: new Date(),
       },
       quoteFinanceProduct: {
         financeProduct: {
@@ -152,14 +159,8 @@ export class QuoteService {
     const model = new QuoteModel(data, detailedQuote);
     model.setIsSync(true);
 
-    let obj: Quote;
-
-    if (quoteId) {
-      obj = await this.quoteModel.findByIdAndUpdate(quoteId, model, { new: true });
-    } else {
-      obj = new this.quoteModel(model);
-      await obj.save();
-    }
+    const obj = new this.quoteModel(model);
+    await obj.save();
 
     return OperationResult.ok(new QuoteDto(obj.toObject())) as any;
   }
@@ -230,6 +231,161 @@ export class QuoteService {
     }
   }
 
+  async getLatestQuote(data: CreateQuoteDto, quoteId?: string): Promise<OperationResult<QuoteDto>> {
+    const foundQuote = await this.quoteModel.findById(quoteId);
+
+    if (!foundQuote) {
+      throw ApplicationException.EnitityNotFound(quoteId);
+    }
+
+    const systemDesign = await this.systemDesignService.getOneById(data.systemDesignId);
+
+    const quoteCostCommon = {
+      markup: 0,
+      discountDetails: [{ amount: 0, description: '' }],
+      netCost: 0,
+    };
+
+    const {
+      panel_quote_details,
+      inverter_quote_details,
+      storage_quote_details,
+      adder_quote_details,
+    } = foundQuote.detailed_quote.quote_cost_buildup;
+
+    const oldPanels = panel_quote_details.reduce((acc, item) => ({ [item.panel_model_id]: item }), {});
+    const oldInverters = inverter_quote_details.reduce((acc, item) => ({ [item.inverter_model_id]: item }), {});
+    const oldStorage = storage_quote_details.reduce((acc, item) => ({ [item.storage_model_id]: item }), {});
+    const oldAdders = adder_quote_details.reduce((acc, item) => ({ [item.adder_model_id]: item }), {});
+
+    const quoteCostBuildup = {
+      panelQuoteDetails: this.groupData(
+        systemDesign.roof_top_design_data.panel_array.map(item => ({
+          panelModelId: item.panel_model_id,
+          panelModelDataSnapshot: item.panel_model_data_snapshot,
+          panelModelSnapshotDate: new Date(),
+          quantity: item.number_of_panels,
+          ...quoteCostCommon,
+          ...pick(oldPanels[item.panel_model_id], ['markup', 'discountDetails', 'netCost']),
+          cost: item.number_of_panels * item.panel_model_data_snapshot.price,
+        })),
+        'panelModelId',
+      ),
+      inverterQuoteDetails: this.groupData(
+        systemDesign.roof_top_design_data.inverters.map(item => ({
+          inverterModelId: item.inverter_model_id,
+          inverterModelDataSnapshot: item.inverter_model_data_snapshot,
+          inverterModelSnapshotDate: new Date(),
+          quantity: item.quantity,
+          ...quoteCostCommon,
+          ...pick(oldInverters[item.inverter_model_id], ['markup', 'discountDetails', 'netCost']),
+          cost: item.quantity * item.inverter_model_data_snapshot.price,
+        })),
+        'inverterModelId',
+      ),
+      storageQuoteDetails: this.groupData(
+        systemDesign.roof_top_design_data.storage.map(item => ({
+          storageModelId: item.storage_model_id,
+          storageModelDataSnapshot: item.storage_model_data_snapshot,
+          storageModelSnapshotDate: new Date(),
+          quantity: item.quantity,
+          ...quoteCostCommon,
+          ...pick(oldStorage[item.storage_model_id], ['markup', 'discountDetails', 'netCost']),
+          cost: item.quantity * item.storage_model_data_snapshot.price,
+        })),
+        'storageModelId',
+      ),
+      adderQuoteDetails: this.groupData(
+        systemDesign.roof_top_design_data.adders.map(item => ({
+          adderModelId: item.adder_id,
+          adderModelDataSnapshot: item.adder_model_data_snapshot,
+          adderModelSnapshotDate: new Date(),
+          quantity: item.quantity,
+          ...quoteCostCommon,
+          ...pick(oldAdders[item.adder_id], ['markup', 'discountDetails', 'netCost']),
+          cost: item.quantity * item.adder_model_data_snapshot.price,
+        })),
+        'adderModelId',
+      ),
+      overallMarkup: 0,
+      totalProductCost: 0,
+      laborCost: {
+        laborCostDataSnapshot: {
+          calculationType: 'standard',
+          unit: 'each',
+        },
+        laborCostSnapshotDate: new Date(),
+        ...quoteCostCommon,
+        netCost: 0,
+      },
+      grossAmount: 0,
+    };
+
+    quoteCostBuildup.totalProductCost = this.calculateTotalProductCost(quoteCostBuildup);
+    quoteCostBuildup.grossAmount = quoteCostBuildup.totalProductCost - quoteCostBuildup.laborCost.netCost;
+
+    const utilityProgram = await this.utilityProgramService.getDetail(data.utilityProgramId);
+    const fundingSource = await this.fundingSourceService.getDetail(data.fundingSourceId);
+
+    const detailedQuote = {
+      systemProduction: systemDesign.system_production_data,
+      quoteCostBuildup,
+      utilityProgram: {
+        utilityProgramId: utilityProgram?.id,
+        utilityProgramName: utilityProgram?.name,
+        rebateAmount: utilityProgram?.rebate_amount,
+        utility_program_data_snapshot: {
+          id: utilityProgram?.id,
+          name: utilityProgram?.name,
+        },
+        utility_program_data_snapshot_date: new Date(),
+      },
+      quoteFinanceProduct: {
+        financeProduct: {
+          productType: fundingSource.type,
+          fundingSourceId: fundingSource.id,
+          fundingSourceName: fundingSource.name,
+          productAttribute: await this.createProductAttribute(fundingSource.type, quoteCostBuildup.grossAmount),
+        },
+        netAmount: quoteCostBuildup.grossAmount,
+        incentiveDetails: [
+          {
+            unit: 'percentage',
+            unitValue: 0,
+            type: 'program_incentives',
+            appliesTo: '',
+            description: '',
+          },
+        ],
+        rebateDetails: [
+          {
+            amount: 0,
+            type: 'program_rebate',
+            description: '',
+          },
+        ],
+        projectDiscountDetails: [
+          {
+            unit: 'percentage',
+            unitValue: 0,
+            description: '',
+            excludeAdders: false,
+          },
+        ],
+      },
+      savingsDetails: [],
+      quoteName: '',
+      isSelected: false,
+      isSolar: systemDesign.is_solar,
+      isRetrofit: systemDesign.is_retrofit,
+    };
+
+    const model = new QuoteModel(data, detailedQuote);
+    model.setIsSync(true);
+
+    return OperationResult.ok(new QuoteDto(model as any)) as any;
+  }
+
   async getAllQuotes(
     limit: number,
     skip: number,
@@ -285,7 +441,24 @@ export class QuoteService {
         res = await this.calculationService.calculateLeaseQuote(data);
         break;
       case FINANCE_PRODUCT_TYPE.LEASE:
-        res = {} as any;
+        const {
+          quoteFinanceProduct: {
+            financeProduct: { productAttribute },
+          },
+        } = data;
+
+        const product = productAttribute as LoanProductAttributesDto;
+
+        res = this.calculationService.calculateLoanSolver(
+          product.interestRate,
+          product.loanAmount,
+          new Date(),
+          product.loanTerm,
+          18,
+          product.upfrontPayment,
+          18,
+          0.01,
+        ) as any;
         break;
       case FINANCE_PRODUCT_TYPE.LEASE:
         res = {} as any;
