fc59f37e28ccbb7884737099a262efa41ed5889f
Thang Huynh <thanghq@droup.co>
2021-02-03 16:08:46 +0700

    fix: add jwt module

--- src/contacts/contact.module.ts
+++ src/contacts/contact.module.ts
@@ -1,5 +1,7 @@
 import { Global, Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { MongooseModule } from '@nestjs/mongoose';
+import { JwtConfigService } from 'src/authentication/jwt-config.service';
 import { ContractController } from './contact.controller';
 import { CONTACT, ContactSchema } from './contact.schema';
 import { ContactService } from './contact.service';
@@ -7,6 +9,9 @@ import { ContactService } from './contact.service';
 @Global()
 @Module({
   imports: [
+    JwtModule.registerAsync({
+      useClass: JwtConfigService,
+    }),
     MongooseModule.forFeature([
       {
         name: CONTACT,
