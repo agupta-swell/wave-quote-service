3a59f949619690e73c8b88a79cd03bfc805701a5
Phung Nguyen <phungnnl@dgroup.co>
2021-03-03 09:14:29 +0700

    🔨 refactor: update eslint
    - 🔨 refactor allow leadingUnderscore in naming conventions for _id
    
    - 🔨 refactor add no-return-assign to allow for enclosed assignment

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,11 +15,25 @@ module.exports = {
     'import/no-unresolved': 0,
     'import/extensions': 0,
     'class-methods-use-this': 0,
+    // NOTE: disabling eslint camelcase because Typescript is enforcing cases below
     camelcase: 0,
+    'max-len': [
+      'error',
+      {
+        code: 120,
+        tabWidth: 2,
+      },
+    ],
     'no-empty-constructor': 0,
     'no-useless-constructor': 0,
     'import/prefer-default-export': 0,
     'max-classes-per-file': 0,
+    'no-return-assign': [
+      'error',
+      {
+        always: false,
+      },
+    ],
     'no-underscore-dangle': 0,
     'no-shadow': 0,
     'new-cap': 0,
@@ -27,5 +41,48 @@ module.exports = {
     'array-callback-return': 0,
     'prefer-destructuring': 0,
     'no-param-reassign': 0,
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: ['default'],
+        format: ['camelCase', 'snake_case', 'PascalCase'],
+        leadingUnderscore: 'allowSingleOrDouble',
+      },
+    ],
+    '@typescript-eslint/quotes': [
+      'error',
+      'single',
+      {
+        avoidEscape: true,
+      },
+    ],
+    'id-blacklist': [
+      'error',
+      'any',
+      'Number',
+      'number',
+      'String',
+      'string',
+      'Boolean',
+      'boolean',
+      'Undefined',
+      'undefined',
+    ],
+    'id-match': 'error',
+    'import/order': 'error',
+    '@typescript-eslint/tslint/config': [
+      'error',
+      {
+        rules: {
+          'import/no-unresolved': true,
+          'max-len': [
+            true,
+            {
+              limit: 120,
+            },
+          ],
+        },
+      },
+    ],
   },
 };
