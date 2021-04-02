cd71c92b385b7b0bbbae67f06ac2625b6b154be1
Son NT <sonnt2@dgroup.co>
2021-04-02 21:13:35 +0700

    [WAV-306]: comment out wav-281

--- src/docusign-communications/sub-services/docusign-template.service.ts
+++ src/docusign-communications/sub-services/docusign-template.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User, USER } from 'src/users/user.schema';
-import { IDefaultContractor, IDisclosureEsaMapping, IGenericObject, ITabData } from '../typing';
+import { IDefaultContractor, IGenericObject, ITabData } from '../typing';
 import { templateBuilderMap } from './templates';
 
 @Injectable()
@@ -15,21 +15,21 @@ export class DocusignTemplateService {
     defaultContractor: IDefaultContractor,
   ): ITabData {
     const assignedMember = genericObject.opportunity.assignedMember;
-    const user: any = this.userModel.find({ _id: assignedMember });
+    // const user = await this.userModel.findById(assignedMember);
     const templateDataBuilder = templateBuilderMap[docusignTemplateId];
 
     if (!templateDataBuilder) {
       throw new Error(`No mapping for DocuSign template id: ${docusignTemplateId}`);
     }
-    if (!user) {
-      throw new Error(`No User for DocuSign template id: ${docusignTemplateId}`);
-    } else {
-      const disclosureEsa: IDisclosureEsaMapping = {
-        salesPersonFirstLast: `${user.profile.firstName} ${user.profile.firstName} `,
-        hisSale: user.hisNumber,
-      };
-      genericObject.assignedMember = disclosureEsa;
-    }
+    // if (!user) {
+    //   throw new Error(`No User for DocuSign template id: ${docusignTemplateId}`);
+    // } else {
+    //   const disclosureEsa: IDisclosureEsaMapping = {
+    //     salesPersonFirstLast: `${user.profile.firstName} ${user.profile.firstName} `,
+    //     hisSale: user.hisNumber,
+    //   };
+    //   genericObject.assignedMember = disclosureEsa;
+    // }
     const tabs = templateBuilderMap[docusignTemplateId](genericObject, defaultContractor);
     return { textTabs: [tabs] };
   }
