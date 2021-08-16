953672f549f3cea2e333e2e978dc76535db0eb85
An NT <anguyen@swellenergy.com>
2021-08-16 10:50:30 +0700

    [WAV-895] Prevent put empty system design name

--- src/system-designs/req/update-system-design.dto.ts
+++ src/system-designs/req/update-system-design.dto.ts
@@ -1,6 +1,6 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
-import { IsBoolean, IsNumber, IsOptional, IsString, ValidateNested } from 'class-validator';
+import { IsBoolean, IsNotEmpty, IsNumber, IsOptional, IsString, ValidateNested } from 'class-validator';
 import { DESIGN_MODE } from '../constants';
 import { CapacityProductionDataDto, RoofTopDataReqDto } from './sub-dto';
 import { ExistingSolarDataDto } from './sub-dto/existing-solar.dto';
@@ -11,7 +11,7 @@ export class UpdateSystemDesignDto {
   opportunityId: string;
 
   @ApiPropertyOptional()
-  @IsOptional()
+  @IsNotEmpty()
   @IsString()
   name: string;
 
