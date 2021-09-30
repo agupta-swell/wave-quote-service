8226655f5091139c5fd1e70864be488e93ec934d
Vy Tran Long <vytl@dgroup.co>
2021-09-30 17:24:45 +0700

    [WAV-1136] Contract Mapping: Additional Terms ESA

--- src/docusign-communications/sub-services/templates/additional-term-esa.ts
+++ src/docusign-communications/sub-services/templates/additional-term-esa.ts
@@ -1,12 +1,15 @@
 import { ILeaseProductAttributes } from 'src/quotes/quote.schema';
 import { TemplateDataBuilder } from '../../typing';
 
-export const getAdditionalTermEsaData: TemplateDataBuilder = (genericObject) => {
-  const leaseProduct = genericObject?.quote?.quoteFinanceProduct?.financeProduct
-    ?.productAttribute as ILeaseProductAttributes;
-  const obj = {} as any;
-  if (leaseProduct) {
-    obj.ESA_ESC = leaseProduct.rateEscalator;
-  }
-  return obj;
+export const getAdditionalTermEsaData: TemplateDataBuilder = ({ quote, leaseSolverConfig }) => {
+  return {
+    price_per_kwh: `${quote.quoteFinanceProduct.financeProduct.productAttribute.newPricePerKWh}`,
+    rate_escalator: `${
+      (<ILeaseProductAttributes>quote.quoteFinanceProduct.financeProduct.productAttribute).rateEscalator
+    }`,
+    storage_payment: `${
+      (<ILeaseProductAttributes>quote.quoteFinanceProduct.financeProduct.productAttribute).leaseSolverConfigSnapshot
+        ?.storagePayment
+    }`,
+  };
 };
