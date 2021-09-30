fb06a78d110d5f8d390d3c2a55a76b96206f551c
Vy Tran Long <vytl@dgroup.co>
2021-09-30 17:14:30 +0700

    [WAV-1127] add primary owner, co owner fullname

--- src/docusign-communications/sub-services/templates/ca-consumer-guide.ts
+++ src/docusign-communications/sub-services/templates/ca-consumer-guide.ts
@@ -1,5 +1,7 @@
 import { TemplateDataBuilder } from '../../typing';
 
-export const getCaConsumerGuideData: TemplateDataBuilder = ({ assignedMember }) => ({
+export const getCaConsumerGuideData: TemplateDataBuilder = ({ assignedMember, signerDetails }) => ({
   his_number: assignedMember?.hisNumber ?? '',
+  primary_owner_full_name: signerDetails.find(e => e.role === 'Primary Owner')?.fullName ?? '',
+  co_owner_full_name: signerDetails.find(e => e.role === 'Co Owner')?.fullName ?? '',
 });
