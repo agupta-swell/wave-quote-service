bfdf15bd6a2ebfc78588059f68befef0c4630470
Vy Tran Long <vmtran.dev@gmail.com>
2021-12-14 15:05:32 +0700

    [WAV-1225] WQT - Solar Energy System Disclosure Document [HIC]

--- src/docusign-communications/sub-services/templates/solar-energy-system-disclosure-document-hic.ts
+++ src/docusign-communications/sub-services/templates/solar-energy-system-disclosure-document-hic.ts
@@ -1,4 +1,16 @@
-import { DocusignTemplate } from 'src/shared/docusign';
+import { IGenericObject } from 'src/docusign-communications/typing';
+import {
+  DefaultTabTransformation,
+  DefaultTabType,
+  DocusignTemplate,
+  DOCUSIGN_TAB_TYPE,
+  TabValue,
+} from 'src/shared/docusign';
 
 @DocusignTemplate('demo', '52fd1f92-db3a-485b-adcb-c9f452b0bdf4')
-export class SolarEnergySystemDisclosureDocumentHicTemplate {}
+@DefaultTabType(DOCUSIGN_TAB_TYPE.PRE_FILLED_TABS)
+@DefaultTabTransformation('snake_case')
+export class SolarEnergySystemDisclosureDocumentHicTemplate {
+  @TabValue<IGenericObject>(({ quote }) => quote.quoteCostBuildup.projectGrandTotal.netCost.toFixed(2))
+  totalCost: number;
+}
