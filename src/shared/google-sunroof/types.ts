0a457356dd22667ce0a60333627595d42aae5834
jsnd <jazondanielz@gmail.com>
2022-03-15 12:23:48 -0400

    [WAV-1688] added file generation dependencies from POC

---
+++ src/shared/google-sunroof/types.ts
@@ -0,0 +1,25 @@
+
+export type Color = [red: number, green: number, blue: number, alpha?: number];
+
+export type Vector2 = [x: number, y: number];
+export type Pixel = Vector2;
+export type PixelPolygon = Pixel[]
+
+export type LatLng = {
+  lat: number,
+  lng: number,
+}
+
+export type LatLngPolygon = LatLng[]
+export type Panel = [LatLng, LatLng, LatLng, LatLng]
+
+export type SystemDesign = {
+  latitude: number,
+  longitude: number,
+  roof_top_design_data: {
+    panel_array: {
+      bound_polygon: LatLngPolygon,
+      panels: Panel[],
+    }[],
+  },
+}
