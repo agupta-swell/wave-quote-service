31f3c77ffea946a09b2106f3ad3604729e06ae6f
Son NT <sonnt2@dgroup.co>
2022-08-02 16:09:14 +0700

    [WAV-2028] mark hourlyUsage as optional

--- src/utilities/req/sub-dto/computed-usage.dto.ts
+++ src/utilities/req/sub-dto/computed-usage.dto.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
-import { IsArray, IsNumber, ValidateNested } from 'class-validator';
+import { IsArray, IsNumber, IsOptional, ValidateNested } from 'class-validator';
 
 class UsageValue {
   @ApiProperty()
@@ -26,6 +26,7 @@ export class ComputedUsageDto {
   monthlyUsage: UsageValue[];
   
   @ApiProperty({ type: UsageValue, isArray: true })
+  @IsOptional()
   @IsArray()
   @Type(() => UsageValue)
   @ValidateNested({ each: true })
