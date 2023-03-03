e6b8aec70dcda0d687184515c8017b50547ef677
tuandsv <ttran@swellenergy.com>
2023-03-03 13:26:54 +0700

    [WAV-2907] mapping to multiple UUIDs

--- src/docusign-communications/sub-services/templates/terms-and-conditions-hic.ts
+++ src/docusign-communications/sub-services/templates/terms-and-conditions-hic.ts
@@ -1,5 +1,7 @@
 import { DocusignTemplate } from 'src/shared/docusign';
 
+@DocusignTemplate('demo', '78f6ce07-08cd-4fb7-af99-abf1d3edfb35')
 @DocusignTemplate('demo', '1647d671-1a44-405d-97f6-711331e857aa')
+@DocusignTemplate('live', '706666bd-09fb-4321-95b8-bd29a1b56079')
 @DocusignTemplate('live', 'fad322ae-24c7-422c-9b0c-35f7135492be')
 export class TermsAndConditionsHicTemplate {}
