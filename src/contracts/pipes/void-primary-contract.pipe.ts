a2a1a8dc1645f18122cd17d0ed7f5550ba3df221
Nguyen Trong Kha <nguyentrongkha@192.168.2.30>
2023-03-28 17:23:14 +0700

    [WAV-2952] Void Change Order in WQT
    Signed-off-by: Nguyen Trong Kha <nguyentrongkha@192.168.2.30>

--- src/contracts/pipes/void-primary-contract.pipe.ts
+++ src/contracts/pipes/void-primary-contract.pipe.ts
@@ -17,10 +17,6 @@ export class VoidPrimaryContractPipe implements PipeTransform<ObjectId, Promise<
   }
 
   private static validate(contract: Contract) {
-    if (![CONTRACT_TYPE.PRIMARY_CONTRACT, CONTRACT_TYPE.GRID_SERVICES_PACKET].includes(contract.contractType)) {
-      throw new BadRequestException('This contract is not allowed to void');
-    }
-
     if (contract.contractStatus === PROCESS_STATUS.VOIDED) {
       throw new BadRequestException('This contract has already been voided');
     }
