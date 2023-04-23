1785462dff9afb99929321356171c44d63f3caae
Aditi Dubey <aditi.dubey@centricconsulting.com>
2023-04-24 00:46:48 +0530

    [WAV-2958] remove export code

--- cdk/IAMRoleStack.ts
+++ cdk/IAMRoleStack.ts
@@ -1,5 +1,5 @@
 import { aws_iam as iam } from 'aws-cdk-lib';
-import { App, Stack, StackProps, CfnOutput } from 'aws-cdk-lib';
+import { App, Stack, StackProps } from 'aws-cdk-lib';
 
 
 const {
@@ -13,19 +13,11 @@ export class IAMRoleStack extends Stack {
   constructor(scope: App, id: string, props: StackProps) {
   super(scope, id, props);
 
-    // 👇 Task execution role creation  
-    const execRole = new iam.Role(this, 'MyAppTaskExecutionRole', {
-      assumedBy: new iam.ServicePrincipal('ecs-tasks.amazonaws.com'),
-      roleName: `${company}-${applicationId}-${processId}-${environment}-ecs-task-execution`,
-    })
-    execRole.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('service-role/AmazonECSTaskExecutionRolePolicy'))
-
-
-    // 👇 Create an output object which defines ecr repo name which is deployed
-    new CfnOutput(this, "Myexecutionrole", {
-        value: execRole.roleArn,
-        exportName: `${company}-${applicationId}-${processId}-${environment}-role-arn`,
-      })
-
-    }
+  // 👇 Task execution role creation  
+  const execRole = new iam.Role(this, 'TaskExecutionRole', {
+    assumedBy: new iam.ServicePrincipal('ecs-tasks.amazonaws.com'),
+    roleName: `${company}-${applicationId}-${processId}-${environment}-ecs-task-execution`,
+  })
+  execRole.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('service-role/AmazonECSTaskExecutionRolePolicy'))
+  }
 }
