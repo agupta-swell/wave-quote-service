92fd7204fa3be710b8dbd0ea18fad00e7907740c
Selena Smith <selenasmith@MB-SSMITH.local>
2023-07-13 17:31:56 -0700

    [WAV-3290] clean up token.controller.ts

--- src/tokens/token.controller.ts
+++ src/tokens/token.controller.ts
@@ -1,30 +1,6 @@
-/*
 import { Controller, Get } from '@nestjs/common';
 import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { ServiceResponse } from 'src/app/common';
-import { ToolTipDto } from './res/tool-tip.dto';
-import { ToolTipService } from './tool-tip.service';
-
-@ApiTags('Tool tips')
-@Controller('/tool-tips')
-export class ToolTipController {
-  constructor(private readonly toolTipService: ToolTipService) {}
-
-  @Get()
-  @ApiOperation({ summary: 'Get all tool tips' })
-  @ApiOkResponse({ type: ToolTipDto })
-  async getAllToolTips(): Promise<ServiceResponse<ToolTipDto[]>> {
-    const result = await this.toolTipService.getAllToolTips();
-    return ServiceResponse.fromResult(result);
-  }
-}
-*/
-import { Controller, Get } from '@nestjs/common';
-import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
-import { ServiceResponse } from 'src/app/common';
-//import { ToolTipDto } from './res/tool-tip.dto';
-//import { ToolTipService } from './tool-tip.service';
-
 
 @Controller('/tokens')
 export class TokenController {
