0b00fd9b4dc395cdfdbed11121b419b23861b427
trongkha256 <knguyen@swellenergy.com>
2023-07-20 16:45:51 +0700

    [WAV-3419] [PV PV+ES] ESA Additional Terms
    Signed-off-by: trongkha256 <knguyen@swellenergy.com>

--- src/docusign-communications/sub-services/templates/additional-term-esa.ts
+++ src/docusign-communications/sub-services/templates/additional-term-esa.ts
@@ -1,19 +1,26 @@
-import { ILeaseProductAttributes } from 'src/quotes/quote.schema';
+import { IEsaProductAttributes, ILeaseProductAttributes } from 'src/quotes/quote.schema';
 import {
   DefaultTabTransformation,
   DefaultTabType,
   DocusignTemplate,
   DOCUSIGN_TAB_TYPE,
+  TabLabel,
   TabValue,
 } from 'src/shared/docusign';
 import { IGenericObject } from '../../typing';
 
 @DocusignTemplate('demo', 'c40d9d52-08ba-4c12-9ad2-9f781fb354b4')
+@DocusignTemplate('live', '72f299d9-0745-40ed-afa7-92e0e2ffd075')
 @DefaultTabTransformation('snake_case')
 @DefaultTabType(DOCUSIGN_TAB_TYPE.PRE_FILLED_TABS)
 export class AdditionalTermEsaDataTemplate {
   @TabValue<IGenericObject>(({ quote }) => quote.quoteFinanceProduct.financeProduct.productAttribute.newPricePerKWh)
   pricePerKwh: number;
+
+  @TabValue<IGenericObject>(
+    ({ quote }) => (<IEsaProductAttributes>quote.quoteFinanceProduct.financeProduct.productAttribute).rateEscalator,
+  )
+  esaEsclator: number;
 
   @TabValue<IGenericObject>(
     ({ quote }) => (<ILeaseProductAttributes>quote.quoteFinanceProduct.financeProduct.productAttribute).rateEscalator,
