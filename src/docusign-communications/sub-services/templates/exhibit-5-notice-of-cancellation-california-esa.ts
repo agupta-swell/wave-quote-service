278621a8f202daac9aca5fb2f3ec9477a72922d0
tuandsv <ttran@swellenergy.com>
2023-07-21 16:58:34 +0700

    [WAV-3437] [PV PV+ES ES] Exhibit 5 Notice of Cancellation

--- src/docusign-communications/sub-services/templates/exhibit-5-notice-of-cancellation-california-esa.ts
+++ src/docusign-communications/sub-services/templates/exhibit-5-notice-of-cancellation-california-esa.ts
@@ -1,4 +1,5 @@
 import { DocusignTemplate } from 'src/shared/docusign';
 
 @DocusignTemplate('demo', '1451f1e2-4620-4878-b74b-7bea35bd6f9b')
+@DocusignTemplate('live', '517469b4-7692-442a-ae6b-2abd247c0299')
 export class Exhibit5NoticeOfCancellationCaliforniaESATemplate {}
