90e30baa2aae14313ec157f0c40ff874adc904d6
Long Hoang Liem <lliem@swellenergy.com>
2023-07-21 14:36:06 +0700

    [WAV-3433] {PV PV+ES} Exhibit 3 Customer Obligations

--- src/docusign-communications/sub-services/templates/exhibit-3-customer-system-related-obligations-esa.ts
+++ src/docusign-communications/sub-services/templates/exhibit-3-customer-system-related-obligations-esa.ts
@@ -1,4 +1,5 @@
 import { DocusignTemplate } from 'src/shared/docusign';
 
 @DocusignTemplate('demo', '1884a5a2-7804-48bc-9cb2-741ad34bbfcb')
+@DocusignTemplate('live', '2ad0a985-c6f9-43cc-b98c-f160bffb90b1')
 export class Exhibit3CustomerSystemRelatedObligationsESATemplate {}
