522e3f2b6fb7cfb6ddb0a1694a84a4157ba1eb05
DucDao <ducdv@dgroup.co>
2023-07-21 16:24:17 +0700

    [WAV-3443] [PV PV+ES] Exhibit 9 Insurance

--- src/docusign-communications/sub-services/templates/exhibit-9-insurance-esa.ts
+++ src/docusign-communications/sub-services/templates/exhibit-9-insurance-esa.ts
@@ -1,4 +1,5 @@
 import { DocusignTemplate } from 'src/shared/docusign';
 
 @DocusignTemplate('demo', '18d5b637-49d9-4446-827c-13fa82179529')
+@DocusignTemplate('live', '4ed7fca8-e762-4ccb-82c7-4ea62a873fe7')
 export class Exhibit9InsuranceESATemplate {}
