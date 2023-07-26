7da0a129fc207f70eff87b81017e1f8d5a2982bf
Dinh Son Hai <hdinh@swellwenergy.com>
2023-07-26 13:52:58 +0700

    [WAV-3445] [PV PV+ES] Exhibit 10 Payment Schedule

--- src/docusign-communications/sub-services/templates/exhibit-10-payment-schedule-esa.ts
+++ src/docusign-communications/sub-services/templates/exhibit-10-payment-schedule-esa.ts
@@ -1,62 +1,91 @@
-import { sum } from 'lodash';
+/* eslint-disable no-restricted-properties */
+import BigNumber from 'bignumber.js';
 import { IGenericObject } from 'src/docusign-communications/typing';
-import { ILeaseProductAttributes } from 'src/quotes/quote.schema';
+import { FINANCE_PRODUCT_TYPE } from 'src/quotes/constants';
+import { IEsaProductAttributes } from 'src/quotes/quote.schema';
 import { DefaultTabType, DocusignTemplate, DOCUSIGN_TAB_TYPE, TabDynamic } from 'src/shared/docusign';
+import { Number2DecimalsFormatter } from 'src/utils/numberFormatter';
 
 @DocusignTemplate('demo', 'd7758c3f-82bc-4d7d-93f2-2c939c661f18')
+@DocusignTemplate('live', '6e506965-14c4-4d82-a651-a2b608b1ea4f')
 @DefaultTabType(DOCUSIGN_TAB_TYPE.PRE_FILLED_TABS)
 export class Exhibit10PaymentScheduleEsaTemplate {
   @TabDynamic<IGenericObject>(genericObject => {
-    const { quote, leaseSolverConfig } = genericObject;
+    const { quote } = genericObject;
     const result: Record<string, string> = {};
+    const DEGRADATION_RATE = 0.005;
+    const BUYOUT_DISCOUNT = 0.03;
+    const DEFAULT_ESA_TERM = 25;
 
-    result.avg_solar_rate_per_kwh_year_1 = (quote.systemProduction.annualUsageKWh / 12).toFixed(2);
+    const yearOneProduction = quote.systemProduction.generationKWh;
 
-    const leaseAttribute =
-      quote.quoteFinanceProduct.financeProduct.productType === 'lease'
-        ? (quote.quoteFinanceProduct.financeProduct.productAttribute as ILeaseProductAttributes)
+    const esaAttribute =
+      quote.quoteFinanceProduct.financeProduct.productType === FINANCE_PRODUCT_TYPE.ESA
+        ? (quote.quoteFinanceProduct.financeProduct.productAttribute as IEsaProductAttributes)
         : undefined;
 
-    const term = leaseAttribute?.leaseTerm ?? 0;
+    const escalatorRate = esaAttribute?.rateEscalator ?? 0;
+    result.esa_escalator = `${(escalatorRate * 100).toFixed(2)}`;
 
-    const esclatorRate = leaseAttribute?.rateEscalator ?? 0;
+    const term = esaAttribute?.esaTerm ?? 0;
 
-    result.storage_monthly_payment = `${leaseSolverConfig?.storagePayment || 0}`;
+    const monthlyPayment = esaAttribute?.grossFinancePayment ?? 0;
+    const basePricePerkWhRate = new BigNumber(yearOneProduction ? (monthlyPayment * 12) / yearOneProduction : 0)
+      .decimalPlaces(2)
+      .toNumber();
+    result.price_per_kwh = Number2DecimalsFormatter.format(basePricePerkWhRate);
 
-    result.ESA_ESC = `${esclatorRate}`;
+    const NPV = (cashflow: number[], discountRate: number): number =>
+      cashflow.reduce((acc, val, i) => acc + val / Math.pow(1 + discountRate, i), 0);
 
-    const initMonth = leaseAttribute?.yearlyLeasePaymentDetails[0].monthlyPaymentDetails[0].month || 0;
+    const getCashflow = (originalArray: number[]): number[] => {
+      const cashflow: number[] = [];
 
-    const leanYearlyPayments = leaseAttribute
-      ? leaseAttribute.yearlyLeasePaymentDetails.reduce<number[][]>((acc, cur, idx, arr) => {
-          const curYear = cur.monthlyPaymentDetails.filter(e => e.month >= initMonth).map(e => e.paymentAmount);
-          const nextYear = arr[idx + 1]
-            ? arr[idx + 1].monthlyPaymentDetails.filter(e => e.month < initMonth).map(e => e.paymentAmount)
-            : [];
+      originalArray.forEach(value => {
+        cashflow.push(...Array.from({ length: 12 }, () => value));
+      });
 
-          acc.push([...curYear, ...nextYear]);
-          return acc;
-        }, [])
-      : [];
+      return cashflow;
+    };
 
-    // Temporarily using fixed value
-    const annualDiscountRate = 0.03;
+    const estimatedMonthlyPaymentList: number[] = [];
 
-    [...Array(term)].forEach((_, idx) => {
+    [...Array(DEFAULT_ESA_TERM)].forEach((_, idx) => {
       const yearIdx = idx + 1;
 
-      result[`ESA_PMT${yearIdx}`] = `${leaseAttribute?.yearlyLeasePaymentDetails[idx].monthlyPaymentDetails[0] ?? 0}`;
-
-      if (idx > 5) {
-        result[`Yr${yearIdx}_Payoff`] = `${leanYearlyPayments.reduce<number>((acc, cur, id) => {
-          if (id > idx) {
-            acc += sum(cur.map((v, j) => v / (1 + annualDiscountRate / 12) ** j));
-          }
-
-          return acc;
-        }, 0)}`;
+      if (yearIdx > term && term < DEFAULT_ESA_TERM) {
+        estimatedMonthlyPaymentList.push(0);
+        result[`payment_year${yearIdx}`] = '$0.00';
+        return;
       }
+
+      const estimatedProduction = yearOneProduction * Math.pow(1 - DEGRADATION_RATE, yearIdx - 1);
+      const pricePerkWhRate = basePricePerkWhRate * Math.pow(1 + escalatorRate, yearIdx - 1);
+
+      const estimatedMonthlyPayment = (estimatedProduction * pricePerkWhRate) / 12;
+
+      estimatedMonthlyPaymentList.push(estimatedMonthlyPayment);
+
+      result[`payment_year${yearIdx}`] = `$${Number2DecimalsFormatter.format(estimatedMonthlyPayment)}`;
     });
+
+    estimatedMonthlyPaymentList.forEach((_, index) => {
+      const yearIdx = index + 1;
+
+      if (yearIdx < 6) {
+        return;
+      }
+
+      if (yearIdx > term && term < DEFAULT_ESA_TERM) {
+        result[`payoff_year_${yearIdx}`] = '$0.00';
+        return;
+      }
+
+      result[`payoff_year_${yearIdx}`] = `$${Number2DecimalsFormatter.format(
+        NPV(getCashflow(estimatedMonthlyPaymentList.slice(index, term)), BUYOUT_DISCOUNT / 12),
+      )}`;
+    });
+
     return result;
   })
   dynamicTabs: unknown;
