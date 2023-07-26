0cd69fd573d60c44177d80eb9779ba548571709a
Selena Walsh Smith <133678230+ssmith-swell@users.noreply.github.com>
2023-07-26 00:48:13 -0700

    [WAV-3434] docusign: exhibit 3 customer obligations (#809)
    * [WAV-3434] docusign: exhibit 3 customer obligations
    
    * [WAV-3434] [ES] Exhibit 3 Customer Obligations
    
    * [WAV-3434] rearrange import
    
    ---------
    
    Co-authored-by: Selena Smith <selenasmith@MB-SSMITH.local>
    Co-authored-by: HuynhNgocBao <bhuynh@swellenergy.com>

---
+++ src/docusign-communications/sub-services/templates/exhibit-3-customer-system-related-obligations-esa-es-only.ts
@@ -0,0 +1,5 @@
+import { DocusignTemplate } from 'src/shared/docusign';
+
+@DocusignTemplate('demo', '94df351f-f51f-4dbc-b083-b498911ff61c')
+@DocusignTemplate('live', 'c256aa8d-181c-450b-ba2e-5962c9a367e0')
+export class Exhibit3CustomerSystemRelatedObligationsESAESOnlyTemplate {}
--- src/docusign-communications/sub-services/templates/index.ts
+++ src/docusign-communications/sub-services/templates/index.ts
@@ -16,6 +16,7 @@ import './energy-home-improvement-agreement-hic';
 import './exhibit-1-swell-services-esa';
 import './exhibit-2-limited-warranties-and-manufacturer-warranties-esa';
 import './exhibit-3-customer-system-related-obligations-esa';
+import './exhibit-3-customer-system-related-obligations-esa-es-only';
 import './exhibit-4-arbitration-clause-esa';
 import './exhibit-5-notice-of-cancellation-california-esa';
 import './exhibit-6-state-disclosures-california-esa';
